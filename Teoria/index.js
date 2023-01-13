ef516faaa0535cf8054ca52f260298dc50ab40fe
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2023-01-13 16:49:49 -0500

    Asignación de Variables

--- Teoria/index.js
+++ Teoria/index.js
@@ -3,5 +3,5 @@ console.log('Hola Mundo');
 /** Asignación de valores */
 let variable = "La respuesta es"; //Valor que va a cambiar
 const numb = 5; //Valor no cambia
-const suma = num + numb; //Valor no cambia
+const suma = numb + numb; //Valor no cambia
 console.log(variable + " " + suma);
