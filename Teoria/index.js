4e1f32b381f982d7fc9cda0ff6193ec1efb5b486
Jenner Acosta Diaz <jenner.acosta@itware.com.pe>
2023-01-13 16:53:43 -0500

    Operadores Aritmeticos

--- Teoria/index.js
+++ Teoria/index.js
@@ -3,5 +3,14 @@ console.log('Hola Mundo');
 /** Asignación de valores */
 let variable = "La respuesta es"; //Valor que va a cambiar
 const numb = 5; //Valor no cambia
-const suma = numb + numb; //Valor no cambia
-console.log(variable + " " + suma);
+const operacion = numb + numb; //Valor no cambia
+
+/** Operadores Aritméticos */
+let suma = numb + numb;
+let resta = numb - numb;
+let multiplicacion = numb * numb;
+let divicion = numb / numb;
+let potencia = Math.pow(numb, 2);
+let modulo = numb % numb;
+
+console.log(variable + " " + potencia);
